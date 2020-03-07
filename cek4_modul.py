7b74a388b5b32505686c05d606309b3025c5b35a
Salim Suprayogi <khan.razes@gmail.com>
2020-03-08 00:24:56 +0700

    delete file program pertama untuk dirapikan kembali agar mudah di baca

--- cek4_modul.py
+++
@@ -1,9 +0,0 @@
-def hitungBB(sts, jenis):
-    if jenis == "bmi":
-        r = (int(sts) - 100) - (10 / 100 * (int(sts) - 100))
-        print("\n")
-        print("Berat badan ideal kamu adalah : ", r, "kg")
-    elif jenis == "bmiW":
-        rm4 = (int(sts)- 100)-(15 / 100 *(int(sts) - 100))
-        print("\n")
-        print("Berat badan ideal kamu adalah : ", rm4, "kg")
